57f5240a38615f90289c9648ef7cc2d3707b677a
youssef el ouidad <y.elouidad@gmail.com>
2023-12-06 22:52:21 +0100

    my first commit

---
+++ models/base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a class BaseModel that defines all
+attributes and methodes for other classes """
+
+from datetime import datetime
+from uuid import uuid4
+from models import storage
+
+
+class BaseModel:
+    """the desined BaseModel class """
+    pass
